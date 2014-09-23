fa94e677b2f9beb1ab3ba3687c69bc5d41b8d31e
Kinga <kinga.giza.10@gmail.com>
2014-09-23 22:05:45 +0200

    switch

---
+++ switch.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf ( "%i", &n );
+
+	switch ( n )
+	{
+		case 1:
+			printf ( "jeden \n" );
+
+
+		case 2:
+			printf ( "dwa \n" );
+
+
+		default:
+			printf ( "x \n" );
+	}
+
+
+	return 0;
+}
+
