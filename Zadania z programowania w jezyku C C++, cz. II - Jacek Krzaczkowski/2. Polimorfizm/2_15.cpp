b902d0e7dc910cef6c6d39f062eba3bdeb168878
Damian Rzeszot <damian.rzeszot@gmail.com>
2016-04-29 00:13:49 +0200

    Zadania do zrobienia

---
+++ Zadania z programowania w jezyku C C++, cz. II - Jacek Krzaczkowski/2. Polimorfizm/2_15.cpp
@@ -0,0 +1,18 @@
+/*
+ Zbiór zadań z programowania w języku C/C++ cz. 2
+ Jacek Krzaczkowski
+
+ Zadanie 2.15
+
+ Zaimplementuj klasę `czworokat` posiadającą pola chronione `a`, `b`, `c` i `d`
+ służące do przechowywania długości boków czworokąta.
+ Klasa czworokat powinna posiadać:
+  — metodę `wypisz` wypisującą na standardowym wyjściu długości wszystkich czterech boków,
+  — czysto wirtualną metodę `pole` zwracającą jako wartość pole czworokąta,
+  — czteroargumentowy konstruktor nadający polom `a`, `b`, `c` i `d` wartości otrzymane w argumentach.
+*/
+
+// typ a,b,c,d -> double
+// chronione -> `protected`
+// czysto wirtualna -> `virtual ... = 0`
+// Konstruktor -> http://edu.pjwstk.edu.pl/wyklady/pro/scb/PRG2CPP_files/node103.html
