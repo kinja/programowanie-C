e210fbf7a836d4b5bff81dcd9f2890607a0895fd
Damian Rzeszot <damian.rzeszot@gmail.com>
2016-04-28 22:01:41 +0200

    zadania do zrobienia

---
+++ Zadania z programowania w jezyku C C++, cz. II - Jacek Krzaczkowski/2. Polimorfizm/2_2.cpp
@@ -0,0 +1,13 @@
+/*
+ Zbiór zadań z programowania w języku C/C++ cz. 2
+ Jacek Krzaczkowski
+
+ Zadanie 2.2
+ 
+ Napisz klasę `liczba` służącą do przechowywania liczb wymiernych.
+ Klasa `liczba` powinna posiadać publiczne pole `re` typu `double` oraz następujące metody publiczne:
+    — wirtualną metodę `modul` zwracającą moduł przechowywanej liczby,
+    — `wieksza` otrzymującą w argumencie referencję `ref` do obiektu klasy `liczba` i zwracający jako wartość
+      `true` jeżeli moduł liczby `ref` jest większy od modułu liczby przechowywanej w obiekcie, na rzecz którego wywoływana 
+      jest metoda oraz `false` w przeciwnym wypadku.
+*/
---
+++ Zadania z programowania w jezyku C C++, cz. II - Jacek Krzaczkowski/2. Polimorfizm/2_4.cpp
@@ -0,0 +1,9 @@
+/*
+ Zbiór zadań z programowania w języku C/C++ cz. 2
+ Jacek Krzaczkowski
+
+ Zadanie 2.4
+
+ Zaimplementuj klasę `funkcja` posiadającą publiczne pole `x` oraz czysto wirtualną metodę `wartosc`,
+ która w klasach pochodnych będzie zwracała wartość funkcji przechowywanej w obiekcie w punkcie `x`.
+*/
