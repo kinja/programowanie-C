5f14f797daf8dbe071034cd18d58d9bc3886a894
Kinga Giza <kinga.giza.10@gmail.com>
2016-05-02 17:34:58 +0200

    Poprawki

--- Zadania z programowania w jezyku C C++, cz. II - Jacek Krzaczkowski/2. Polimorfizm/2_16.cpp
+++ Zadania z programowania w jezyku C C++, cz. II - Jacek Krzaczkowski/2. Polimorfizm/2_16.cpp
@@ -1,3 +1,23 @@
+/*
+Zadanie 2.16
+
+Zaimplementuj następujące klasy pochodne klasy czworokat z zadania
+2.15:
+— prostokat posiadającą następujące publiczne metody:
+— dwuargumentowy konstruktor nadający polom a i c wartość otrzymaną w pierwszym argumencie,zaś polom b i d wartość otrzyma-
+ną w drugim argumencie,
+— wymiary, która otrzymuje dwa argumenty w1 oraz w2 i nadaje
+polom a i c wartość w1, zaś polom b i d wartość w2.
+— kwadrat posiadającą następujące publiczne metody:
+— jednoargumentowy konstruktor nadający polom a, b, c i d war-
+tość otrzymaną w argumencie.
+— wymiar nadającą polom a, b, c i d jedną wartość otrzymaną
+w argumencie.
+Klasy prostokat i kwadrat powinny mieć odpowiednio przeciążoną me-
+todę pole.
+*/
+
+
 class prostokat : public czworokat
 {
 public:
--- Zadania z programowania w jezyku C C++, cz. II - Jacek Krzaczkowski/2. Polimorfizm/2_17.cpp
+++ Zadania z programowania w jezyku C C++, cz. II - Jacek Krzaczkowski/2. Polimorfizm/2_17.cpp
@@ -1,3 +1,12 @@
+/*
+Zadanie 2.17
+
+Napisz funkcję wypisz_pola, która otrzymuje jako argument tablicę
+o elementach typu czworokat *, gdzie czworokat jest typem zdefi-
+niowanym w zadaniu 2.15 oraz jej rozmiar i wypisuje pola wszystkich
+przechowywanych w tablicy czworokątów.
+*/
+
 void wypisz_pola(czworokat* tab[], int n)
 {
     for (int i = 0; i < 0; i++)
